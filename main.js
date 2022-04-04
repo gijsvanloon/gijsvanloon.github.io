06f0aa9fe55daaedc8382b52f0bbec4647a2901a
gijsvanloon <g.van.loon@sintlucasedu.nl>
2022-04-04 15:38:44 +0200

    4-4 .2

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function copyFunction() {
     navigator.clipboard.writeText('g.van.loon@sintlucasedu.nl');
 }
+
+backgroundImages = ['noise_1', 'noise_2', 'noise_3', 'noise_4', 'noise_5'];
+
+function randomBackground() {
+    randomIndex = Math.floor(Math.random() * backgroundImages.length);
+    
+    currentBackground = backgroundImages[randomIndex];
+
+    document.getElementsByClassName('noise').style.background = "url(/Images/${currentBackground})";
+
+    // https://www.youtube.com/watch?v=1YjybCS4B2U
+    // https://www.youtube.com/watch?v=OiSekG3DHbg
+}
